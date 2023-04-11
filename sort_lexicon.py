50171406161f41bf33573e4f745b40745745177d
Manu Leach <manuleach1@gmail.com>
2023-04-11 17:47:04 +0100

    lexicon sorter

--- sort_lexicon.py
+++ sort_lexicon.py
@@ -4,38 +4,45 @@ Created on Wed Apr  5 14:05:19 2023
 
 @author: manul
 """
-from wordle_solver import load_lexicon_to_list
+from wordle_solver import Lexicon
 
-def sort_repeats_to_bottom(lexicon):
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
+def sort_repeats_to_bottom(word_list):
 
     go_to_bottom = []
 
-    for word in lexicon:
+    for word in word_list:
         for letter in word:
             if word.count(letter) > 1:
                 go_to_bottom.append(word)
                 break
 
     for word in go_to_bottom:
-        lexicon.remove(word)
-        lexicon.append(word)
+        word_list.remove(word)
+        word_list.append(word)
 
-    return lexicon
+    return word_list
 
-def write_lexicon_to_file(lexicon_path, lexicon):
+def write_lexicon_to_file(lexicon_path, word_list):
 
     sorted_lexicon_path = lexicon_path[:len(lexicon_path) - 4]+ '_sorted.txt'
     with open(sorted_lexicon_path, 'w') as f:
-        for word in lexicon:
+        for word in word_list:
             f.write(word)
             f.write('\n')
 
+def count_l
+
 def main():
 
-    lexicon_path = '500_words.txt'
-    lexicon = load_lexicon_to_list(lexicon_path)
+    lexicon_path = '100_words.txt'
+    lexicon = Lexicon()
+    lexicon.load_from_txt(lexicon_path)
 
-    new_lexicon = sort_repeats_to_bottom(lexicon.copy())
+    new_lexicon = Lexicon()
+    new_lexicon.word_list = sort_by_letter_freq(new_lexicon.word_list)
+
 
     write_lexicon_to_file(lexicon_path, new_lexicon)
 
